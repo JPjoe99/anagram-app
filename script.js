a8897e9ec0dad6f829ca510a88fdb6ff74fb5693
Joseph Price <joe_price195@outlook.com>
2021-02-18 13:34:45 +0000

    commit

--- script.js
+++ script.js
@@ -61,6 +61,7 @@ function addDefinitionsHTML(element, data) {
 function onKeyUp(e) {
     e.preventDefault();
     let enteredWord = e.target.value;
+    console.log(enteredWord);
     return findWordsFromAnagram(enteredWord);
 }
 
